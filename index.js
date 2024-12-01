7ba26b1a74d9f75b37ae2806842f7d347b580074
stamellashroom1 <alex@crpl.com.au>
2024-12-01 12:10:51 +1100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'https://stamellashroom1.github.io'
+}));
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/test', (req, res) => {
+    res.json({
+        message: "Test functional",
+        timestamp: Date.now()
+    });
 });
 
 app.listen(port, () => {
