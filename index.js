fa24868977434492cf33ce44883756fc8735f866
stamellashroom1 <alex@crpl.com.au>
2025-07-29 14:36:51 +1000

    basic upload

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    const timestamp = Date.now();
+    const safeIP = (req.ip || 'unknown').replace(/[:.]/g, "-");
+    const ext = path.extname(file.originalname);
+    cb(null, `${timestamp}-${safeIP}${ext}`);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.use(cors({
     origin: 'https://stamellashroom1.github.io'
@@ -17,6 +33,10 @@ app.get('/test', (req, res) => {
         message: "Test functional",
         timestamp: new Date().toISOString()
     });
+});
+
+app.post('/uploads', upload.single('file'), (req, res) => {
+  res.send("File uploaded successfully.");
 });
 
 app.listen(port, () => {
